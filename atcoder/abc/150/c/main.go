f2eb3c4e4caa8f39cf1d85fab89c4e7d8642f20a
nobishino <nobishino@example.com>
2020-01-17 21:26:45 +0900

    permutation dirty implementation　次は辞書順ソートする

--- atcoder/abc/150/c/main.go
+++ atcoder/abc/150/c/main.go
@@ -4,7 +4,52 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Print("Hello")
+//スクロールされると動く
+//permutations 意味　順列
+//欠陥では...
+//フォローしなければOK
+//フォローってなんだっけ
+//とりあえずファイルエクスプローラから別なファイル開けばフォローされないような気がする　知らんけど
+var permutations [][]int = make([][]int, 0, 10)
 
+//これ今ほかに誰かいるのかな only me
+//See -> Session Details > Participants
+//make sense, well noted
+func main() {
+	fmt.Println("Hello")
+	maxDepth := 4
+	for i := 0; i < maxDepth; i++ {
+		call(i, 0, initialize(maxDepth))
+	}
+	//なんかアレだけど一応順列は出た
+	//辞書順ソートするか
+	fmt.Println(permutations)
+	fmt.Println(len(permutations))
+}
+
+func initialize(length int) (result []int) {
+	result = make([]int, length)
+	for i := 0; i < length; i++ {
+		result[i] = -1
+	}
+	return
+}
+
+//DFSみたいなので順列を求めようとしています
+func call(v int, depth int, path []int) {
+	copiedPath := make([]int, len(path))
+	maxDepth := len(path)
+	copy(copiedPath, path)
+	copiedPath[v] = depth
+	// fmt.Println(copiedPath)
+	if depth == maxDepth-1 {
+		permutations = append(permutations, copiedPath)
+	}
+	for i, w := range copiedPath {
+		if w != -1 {
+			continue
+		} else {
+			call(i, depth+1, copiedPath)
+		}
+	}
 }
