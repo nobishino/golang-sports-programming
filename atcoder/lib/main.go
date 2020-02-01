4bc1e49a07de4a1b3da4e28cd65d2f243e12ceda
nobishino <nobishino@example.com>
2020-02-01 20:16:33 +0900

    Add cp snippet

---
+++ atcoder/lib/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+}
+
+var scanner *bufio.Scanner
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+}
+func nextInt() int {
+	if !scanner.Scan() {
+		panic("No more token.")
+	}
+	num, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		panic("nextInt(): cannot convert to int: " + scanner.Text())
+	}
+	return num
+}
