8cb57f04c474510b51987932a201056f71eb2a41
nobishino <nobishino@example.com>
2020-02-09 11:11:07 +0900

    Add ModPow(base, exponent, modulo int) int

--- atcoder/lib/math/basic.go
+++ atcoder/lib/math/basic.go
@@ -1,6 +1,6 @@
 package math
 
-//MinInt は、2つの整数を受け取り、最小値を返す。
+//MinInt は、2つの整数を受け取り、最小値を返します。
 func MinInt(x, y int) int {
 	if x < y {
 		return x
@@ -9,11 +9,33 @@ func MinInt(x, y int) int {
 	}
 }
 
-//MaxInt は、2つの整数を受け取り、最大値を返す。
+//MaxInt は、2つの整数を受け取り、最大値を返します。
 func MaxInt(x, y int) int {
 	if x < y {
 		return y
 	} else {
 		return x
 	}
+}
+
+//ModPow は整数の累乗関数で、剰余もサポートします。
+//base^exponentの値をmoduleで割った余りを返します。
+//moduloが1以下の場合には、剰余演算をしません。
+//baseが負の値である場合には、返す値が負になることがあります。
+func ModPow(base, exponent, modulo int) (result int) {
+	result = 1
+	for exponent > 0 {
+		if exponent%2 == 1 {
+			result *= base
+			if modulo > 1 {
+				result %= modulo
+			}
+		}
+		base *= base
+		if modulo > 1 {
+			base %= modulo
+		}
+		exponent /= 2
+	}
+	return
 }
--- atcoder/lib/math/basic_test.go
+++ atcoder/lib/math/basic_test.go
@@ -1,6 +1,9 @@
 package math
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExampleMinInt() {
 	fmt.Println(MinInt(3, 4))
@@ -12,4 +15,29 @@ func ExampleMaxInt() {
 	fmt.Println(MaxInt(3, 4))
 	//Output:
 	//4
+}
+
+func ExampleModPow() {
+	fmt.Println(ModPow(5, 7, 13))
+	fmt.Println(ModPow(5, 7, 2))
+	fmt.Println(ModPow(5, 7, 1))
+	//Output:
+	//8
+	//1
+	//78125
+}
+
+func TestModPow1(t *testing.T) {
+	if ModPow(3, 400, 1000000007) != 978888738 {
+		t.Fatalf("ModPow(3,400,1000000007) should be %d, actual value = %d", 978888738, ModPow(3, 400, 1000000007))
+	}
+}
+func TestModPow2(t *testing.T) {
+	if ModPow(1234567, 9876543210, 1000000007) != 598562710 {
+		t.Fatal("ModPow() does not work correctly\n")
+	}
+}
+
+func BenchmarkModPow(b *testing.B) {
+	ModPow(7891, 123456789, 1000000007)
 }
