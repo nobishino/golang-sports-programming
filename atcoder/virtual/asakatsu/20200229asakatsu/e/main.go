e433ed00c8b10eaab647fee40a51d5a7af8bf575
nobishino <nobishino@example.com>
2020-03-01 14:28:41 +0900

    Join 20200301asakatsu

---
+++ atcoder/virtual/asakatsu/20200229asakatsu/e/main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var (
+	N     int
+	D, f  [300][300]int
+	Links LList
+	LL    [300][]Link
+)
+
+func main() {
+	readVariables()
+	fmt.Println(Links)
+}
+
+func readVariables() {
+	N = nextInt()
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			weight := nextInt()
+			if i >= j {
+				continue
+			}
+			Links = append(Links, Link{i, j, weight})
+		}
+	}
+	sort.Sort(Links)
+}
+
+type LList []Link
+
+func (l LList) Len() int           { return len(l) }
+func (l LList) Less(i, j int) bool { return l[i].weight < l[j].weight }
+func (l LList) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+
+type Link struct {
+	from   int
+	to     int
+	weight int
+}
+
+/* Template */
+
+var scanner *bufio.Scanner
+
+func init() {
+	Max := 1001001
+	scanner = bufio.NewScanner(os.Stdin)
+	scanner.Buffer(make([]byte, 0, Max), Max)
+	scanner.Split(bufio.ScanWords)
+}
+
+//nextInt converts next token from stdin and returns integer value.
+//nextInt panics when conversion into an integer fails.
+func nextInt() int {
+	if !scanner.Scan() {
+		panic("No more token.")
+	}
+	num, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		panic("nextInt(): cannot convert to int: " + scanner.Text())
+	}
+	return num
+}
+
+func nextStr() string {
+	if !scanner.Scan() {
+		panic("No more token.")
+	}
+	return scanner.Text()
+}
+
+// MinInt returns minimum argument
+func MinInt(x, y int) int {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+
+//MaxInt returns maximum argument
+func MaxInt(x, y int) int {
+	if x < y {
+		return y
+	} else {
+		return x
+	}
+}
+
+//AbsInt returns |x| for x
+func AbsInt(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+//ModPow calculates integer power with modulo operation
+//if modulo <= 1, it powers w/o module operation
+//if base < 0, return value might be negative too.
+func ModPow(base, exponent, modulo int) (result int) {
+	result = 1
+	for exponent > 0 {
+		if exponent%2 == 1 {
+			result *= base
+			if modulo > 1 {
+				result %= modulo
+			}
+		}
+		base *= base
+		if modulo > 1 {
+			base %= modulo
+		}
+		exponent /= 2
+	}
+	return
+}
+
+//Gcd
+func Gcd(vals ...int) (result int) {
+	if len(vals) == 0 {
+		return
+	}
+	result = vals[0]
+	for i := 1; i < len(vals); i++ {
+		result = gcd(result, vals[i])
+	}
+	return
+}
+
+func gcd(x, y int) int {
+	x, y = AbsInt(x), AbsInt(y)
+	for y > 0 {
+		x, y = y, x%y
+	}
+	return x
+}
+
+//Lcm
+func Lcm(vals ...int) (result int) {
+	if len(vals) == 0 {
+		return
+	}
+	result = vals[0]
+	for i := 1; i < len(vals); i++ {
+		result = lcm(result, vals[i])
+	}
+	return
+}
+
+func lcm(x, y int) int {
+	return x * y / gcd(x, y)
+}
