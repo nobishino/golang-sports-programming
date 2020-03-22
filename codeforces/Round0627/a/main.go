94e02596afed5d6de3317ebbd256ba56dd6a2b2f
nobishino <nobishino@example.com>
2020-03-22 20:47:56 +0900

    Solve CodeforcesR627-A

---
+++ codeforces/Round0627/a/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var (
+	t int
+	n int
+	a []int
+)
+
+func main() {
+	readVariables()
+	for i := 0; i < t; i++ {
+		readQuery()
+		if judge() {
+			fmt.Println("YES")
+		} else {
+			fmt.Println("NO")
+		}
+	}
+}
+
+func judge() bool {
+	if n == 1 {
+		return true
+	}
+	for i := 1; i < n; i++ {
+		if (a[i]-a[i-1])%2 != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func readVariables() {
+	t = nextInt()
+}
+func readQuery() {
+	n = nextInt()
+	a = make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
+	}
+}
+
+/* Template */
+
+var scanner *bufio.Scanner
+
+func init() {
+	Max := 1001001
+	scanner = bufio.NewScanner(os.Stdin)
+	scanner.Buffer(make([]byte, 0, Max), Max)
+	scanner.Split(bufio.ScanWords)
+}
+
+//nextInt converts next token from stdin and returns integer value.
+//nextInt panics when conversion into an integer fails.
+func nextInt() int {
+	if !scanner.Scan() {
+		panic("No more token.")
+	}
+	num, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		panic("nextInt(): cannot convert to int: " + scanner.Text())
+	}
+	return num
+}
+
+func nextStr() string {
+	if !scanner.Scan() {
+		panic("No more token.")
+	}
+	return scanner.Text()
+}
+
+// MinInt returns minimum argument
+func MinInt(x, y int) int {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+
+//MaxInt returns maximum argument
+func MaxInt(x, y int) int {
+	if x < y {
+		return y
+	} else {
+		return x
+	}
+}
+
+//AbsInt returns |x| for x
+func AbsInt(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+//ModPow calculates integer power with modulo operation
+//if modulo <= 1, it powers w/o module operation
+//if base < 0, return value might be negative too.
+func ModPow(base, exponent, modulo int) (result int) {
+	result = 1
+	for exponent > 0 {
+		if exponent%2 == 1 {
+			result *= base
+			if modulo > 1 {
+				result %= modulo
+			}
+		}
+		base *= base
+		if modulo > 1 {
+			base %= modulo
+		}
+		exponent /= 2
+	}
+	return
+}
+
+//Gcd
+func Gcd(vals ...int) (result int) {
+	if len(vals) == 0 {
+		return
+	}
+	result = vals[0]
+	for i := 1; i < len(vals); i++ {
+		result = gcd(result, vals[i])
+	}
+	return
+}
+
+func gcd(x, y int) int {
+	x, y = AbsInt(x), AbsInt(y)
+	for y > 0 {
+		x, y = y, x%y
+	}
+	return x
+}
+
+//Lcm
+func Lcm(vals ...int) (result int) {
+	if len(vals) == 0 {
+		return
+	}
+	result = vals[0]
+	for i := 1; i < len(vals); i++ {
+		result = lcm(result, vals[i])
+	}
+	return
+}
+
+func lcm(x, y int) int {
+	return x * y / gcd(x, y)
+}
