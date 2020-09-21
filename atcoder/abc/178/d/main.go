a7e66874feb2c7b90e4f2e1ca01ece3d4246258e
nobishino <nobishii11@gmail.com>
2020-09-21 16:53:51 +0900

    Solve ABC178-E

--- atcoder/abc/178/d/main.go
+++ atcoder/abc/178/d/main.go
@@ -7,27 +7,50 @@ import (
 	"strconv"
 )
 
-var dp [3000]int
-var s int
-
 const mod int = 1e9 + 7
+
+var s int
 
 func main() {
 	readVariables()
+	fmt.Println(solve2(s))
+	for i := 1; i <= s; i++ {
+		if solve1(s) != solve2(s) {
+			fmt.Println(s, solve1(s), solve2(s))
+		}
+	}
+}
+
+// O(S^2)
+func solve1(s int) int {
+	var dp [3000]int
 	dp[0] = 1
+	for i := 1; i <= s; i++ {
+		for j := 3; i-j >= 0; j++ {
+			dp[i] += dp[i-j]
+			dp[i] %= mod
+		}
+	}
+	answer := dp[s]
+	return answer
+}
+
+// intended O(S)
+func solve2(s int) int {
+	var dp [3000]int
+	dp[0] = 1
+	get := func(i int) int {
+		if i < 0 {
+			return 0
+		}
+		return dp[i]
+	}
 	for i := 1; i <= s; i++ {
 		dp[i] = dp[i-1] + get(i-3)
 		dp[i] %= mod
 	}
 	answer := get(s) - get(s-1)
-	fmt.Println(answer)
-}
-
-func get(i int) int {
-	if i < 0 {
-		return 0
-	}
-	return dp[i]
+	return answer
 }
 
 func readVariables() {
